f000d95ac83fd6d7a1b4b8e9c18a4de6aa5f003e
SAGEcube <152697259+SAGEcube@users.noreply.github.com>
2025-09-29 13:09:23 +0530

    Add files via upload

---
+++ Day-020/Basic Logic Gates using Mux.v
@@ -0,0 +1,18 @@
+module mux_2_1(
+  input [1:0] i,
+  input select,
+  output y_out
+);
+  assign y_out = select ? i[1] : i[0];
+endmodule
+
+module gates_mux(
+  input a,b,
+  output and_out,
+  output or_not,
+  output not_out
+);
+  mux_2_1 mand({b,1'b0}, a, and_out);
+  mux_2_1 mor({1'b1,b}, a, or_not);
+  mux_2_1 mnot({1'b0,1'b1}, a, not_out);
+endmodule
