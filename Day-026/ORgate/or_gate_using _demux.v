ac7eb116cf7e4e23587472d554770eb6647fa3cc
SAGEcube <152697259+SAGEcube@users.noreply.github.com>
2025-10-06 14:07:21 +0530

    Add files via upload

---
+++ Day-026/ORgate/or_gate_using _demux.v
@@ -0,0 +1,18 @@
+module demux_1_2(
+  input sel , input i , output y0,y1);
+  
+  assign {y0,y1} = sel?{1'b0,i}:{i,1'b0};
+  
+endmodule 
+
+
+module demux_or_gate(
+  input a,b,
+  output or_g
+);
+  wire w0,w1,w2 ;
+  demux_1_2 gate(b,~a,w0,w1);
+  
+  demux_1_2  orgate(w0,1'b1,or_g,w2);
+  
+endmodule 
