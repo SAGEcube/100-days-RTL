47a29772669727c1aa457c9b30700479414f1693
SAGEcube <152697259+SAGEcube@users.noreply.github.com>
2025-10-10 15:59:44 +0530

    Add files via upload

---
+++ Day-030/testbench.v
@@ -0,0 +1,21 @@
+`timescale 1ns / 1ps
+
+module test_bench;
+  reg [7:0]in;
+  wire [2:0]out;
+  
+  priority_encoder dut(in, out);
+  
+  always
+  begin
+  in= $random;
+  #10;
+  end
+  initial 
+     begin
+       $dumpfile("test_bench.vcd");
+       $dumpvars(0,test_bench);
+       $monitor("in: %b  out: %b",in, out);
+     #100 $finish;
+     end
+endmodule
